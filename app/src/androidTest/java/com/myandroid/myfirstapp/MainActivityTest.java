ed1fe850cfa74044aae5042ebec4f0cf49baf5f5
break <break0936@gmail.com>
2017-03-23 16:03:11 +0800

    NumberPicker的操作設定和自動化測試

--- app/src/androidTest/java/com/myandroid/myfirstapp/MainActivityTest.java
+++ app/src/androidTest/java/com/myandroid/myfirstapp/MainActivityTest.java
@@ -1,8 +1,14 @@
 package com.myandroid.myfirstapp;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.NumberPicker;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,9 +16,6 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.replaceText;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
@@ -37,25 +40,19 @@ public class MainActivityTest {
     @Test
     public void checkMaleAge25(){
         selectGender("男");
-        onView(withId(R.id.edtAge))
-                .perform(typeText("25"), closeSoftKeyboard());
-        onView(withId(R.id.btnDoSug))
-                .perform(click());
+        selectAge(25);
 
         onView(withId(R.id.txtResult))
-                .check(matches(withText(R.string.sug_not_hurry)));
+                .check(matches(withText("還不急。")));
     }
 
     @Test
     public void checkFemaleAge32(){
         selectGender("女");
-        onView(withId(R.id.edtAge))
-                .perform(typeText("32"), closeSoftKeyboard());
-        onView(withId(R.id.btnDoSug))
-                .perform(click());
+        selectAge(32);
 
         onView(withId(R.id.txtResult))
-                .check(matches(withText(R.string.sug_get_married)));
+                .check(matches(withText("趕快結婚！")));
     }
 
 
@@ -67,5 +64,37 @@ public class MainActivityTest {
                 .perform(click());
         onData(allOf(is(instanceOf(String.class)), is(gender)))
                 .perform(click());
+    }
+
+    private void selectAge(int age){
+        onView(withId(R.id.numAge))
+                .perform(setNumber(age));
+
+    }
+
+    /// 看起來是沒有比較簡單的行為可以完成這個畫面操作測試
+    public static ViewAction setNumber(final int num) {
+        return new ViewAction() {
+
+            /// 只用setValue無法觸發onValueChange，所以除了給他值之外，再讓他上下滾同樣的長度，讓他滾出來的長度不變，但是有觸發事件
+            @Override
+            public void perform(UiController uiController, View view) {
+                NumberPicker np = (NumberPicker) view;
+                np.setValue(num);
+
+                np.scrollBy(0, -1 * 500);
+                np.scrollBy(0, 500);
+            }
+
+            @Override
+            public String getDescription() {
+                return "Set the passed number into the NumberPicker";
+            }
+
+            @Override
+            public Matcher<View> getConstraints() {
+                return ViewMatchers.isAssignableFrom(NumberPicker.class);
+            }
+        };
     }
 }
--- app/src/main/java/com/myandroid/myfirstapp/MainActivity.java
+++ app/src/main/java/com/myandroid/myfirstapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -13,9 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setNumberPicker();
+    }
+
+    // NumberPicker只能用setListener的方式插方法進去
+    private void setNumberPicker(){
+        NumberPicker numAge = (NumberPicker)findViewById(R.id.numAge);
+        numAge.setMinValue(0);
+        numAge.setMaxValue(100);
+        numAge.setOnValueChangedListener(numPickerAgeOnValueChange);
+        numAge.setValue(20);
     }
 
     public void btnDoSug_Click(View view) {
+        NumberPicker numAge = (NumberPicker)findViewById(R.id.numAge);
+        numAge.scrollBy(0, -1 * 500);
+        numAge.scrollBy(0, 500);
+        // this.doSug();
+    }
+
+    private NumberPicker.OnValueChangeListener numPickerAgeOnValueChange = new NumberPicker.OnValueChangeListener() {
+        @Override
+        public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+            doSug();
+        }
+    };
+
+    private void doSug(){
         String gender = this.getGender();
         int age = this.getAge();
         MarriageSuggestion sug = new MarriageSuggestion();
@@ -28,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int getAge(){
-        return Integer.parseInt(((EditText)findViewById(R.id.edtAge)).getText().toString());
+        return ((NumberPicker)findViewById(R.id.numAge)).getValue();
+        // return Integer.parseInt(((EditText)findViewById(R.id.edtAge)).getText().toString());
     }
 }
